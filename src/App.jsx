57e759dce2bb555b8679266088397667a96b712b
Oscar Miras <oscar.miras@gmail.com>
2023-07-10 09:26:03 +0200

    Iteration 3

--- src/App.jsx
+++ src/App.jsx
@@ -6,31 +6,36 @@ import contacts from "./contacts.json";
 function App() {
 
   // Nos pide el enunciado que creemos una variable de estado para guardar todos los contactos. Copiamos el array de objetos con el operador de spread
-  const [allContacts, setAllContants] = useState([...contacts]);
+  const [index, setIndex] = useState(5);
   const [orderBy, setOrderBy] = useState('name');
 
-  // ¿Cómo ordeno el array de tal manera que si orderBy === 'popularity' entonces ordeno allContacts por su popularidad?
+  const handleNewContact = () => {
+    setIndex(index + 1);
+  }
 
-  // si la variable de estado orderBY === 'name' quiero ordenar los contactos allContacts por nombre
-
+  const visibleContacts = contacts.slice(0, index);
 
   return (
     <div className="App">
       <h1> React IronContacts</h1>
+      <h2> Contacts remaining:  {contacts.length - index}</h2>
+      <button disabled={index == contacts.length} onClick={handleNewContact}>Add New Contact</button>
       <button>By Name</button>
       <button>Popularity</button>
 
       <table>
         <thead>
-          <th>Picture</th>
-          <th>Name</th>
-          <th>Popularity</th>
-          <th>Won Oscar
-          </th>
-          <th>Won Emmy</th>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar
+            </th>
+            <th>Won Emmy</th>
+          </tr>
         </thead>
         <tbody>
-          {allContacts.map(c => (<tr id={c.id}>
+          {visibleContacts.map(c => (<tr key={c.id}>
             <td><img style={{ width: 100 }} src={c.pictureUrl} alt={c.name}></img></td>
             <td>{c.name}</td>
             <td>{c.popularity.toFixed(2)}</td>
